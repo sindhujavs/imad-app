de68a4f9d440af08eaa47036d787511c230d083a
sindhujavs <30454562+sindhujavs@users.noreply.github.com>
2017-08-08 21:59:37 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js',function(req, res){
+    res.sendFile(path.join(__dirname,'ui','main.js'));
+});
+
+app.get('/ui/article-one.html',function(req, res){
+    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+});
+
+app.get('/ui/article-two.html',function(req, res){
+    res.sendFile(path.join(__dirname,'ui','article-two.html'));
+});
+
+app.get('/ui/article-three.html',function(req, res){
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
+});
+
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
